9b7ba3e4690f6ba9ba85eefbdcdaf59e78f58349
Andrin Bertschi <abertschi@abertschi.ch>
2022-08-26 22:07:05 +0200

    clean up

--- app/frontend/vue.config.js
+++ app/frontend/vue.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: process.env.NODE_ENV != 'production',
-
+  assetsDir:  'webapp'
 
 })
--- tools/build-webapp.sh
+++ tools/build-webapp.sh
@@ -9,11 +9,9 @@ set -x
 cd $ROOT_DIR
 npm run build
 
-rm -rf $HUGO_STATIC_ROOT/css
-rm -rf $HUGO_STATIC_ROOT/js
+rm -rf $HUGO_STATIC_ROOT/webapp
 
-cp -rf $ROOT_DIR/dist/css $HUGO_STATIC_ROOT/
-cp -rf $ROOT_DIR/dist/js $HUGO_STATIC_ROOT/
+cp -rf $ROOT_DIR/dist/webapp $HUGO_STATIC_ROOT/
 
 html_file=$ROOT_DIR/dist/index.html
 html=$(sed 's/<head>//' $html_file | sed 's/<\/head>//')
